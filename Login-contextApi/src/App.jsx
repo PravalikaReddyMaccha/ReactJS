58f0ebfd88cfe1be11e9f5f4c55b1cc4c4fb05ec
Pravalika <pravalikareddymaccha@gmail.com>
2025-05-31 00:11:57 +0530

    Changed UserContextProvider

--- Login-contextApi/src/App.jsx
+++ Login-contextApi/src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
+import UserContext from './Context/UserContext';
 import UserContextProvider from './Context/UserContextProvider';
 import Login from './Components/Login.jsx';
 import Profile from './Components/Profile.jsx';
 import './App.css'
 
 function App() {
-
+   const [user,setUser] = useState(null);
+  
   return (
   <>
-    <UserContextProvider>
+    <UserContext.Provider value = {{user,setUser}}>
       <Login/>
       <br/>
       <br/>
       <Profile/>
       {/* <h2 className="bg-red-500">Iam pravalika</h2> */}
-    </UserContextProvider>
+    </UserContext.Provider>
     </>
   )
 }
--- Login-contextApi/src/Context/UserContext.js
+++ Login-contextApi/src/Context/UserContext.js
@@ -1,5 +1,20 @@
 import {createContext} from 'react';
+import {useState} from 'react';
+// import UserContext from './UserContext';
 
 const UserContext = createContext();
 
 export default UserContext;
+
+// function UserContextProvider({children}){
+
+//     const [user,setUser] = useState(null);
+//     return (
+//     <UserContext.Provider value = {{user,setUser}}> 
+//       {children}
+//     </UserContext.Provider>
+//    )
+    
+// }
+
+// export default UserContextProvider;
