978f6e8d8782a157a42ef4220efd8a66fe419656
Pravalika <pravalikareddymaccha@gmail.com>
2025-06-02 16:34:03 +0530

    ToDo-ContextApi-LocalStorage

---
+++ ToDo-ContextApi/src/Context/TodoContext.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {createContext,useContext} from 'react';
+
+export const TodoContext = createContext({
+
+   todos:[
+     {
+        id:1,
+        todo: "Todo mssg",
+        completed:false
+     }
+   ],
+   addTodo : (todo)=>{},
+   updateTodo : (id,todo)=>{},
+   deleteTodo : (id)=>{},
+   toggleComplete :(id)=>{}
+})
+export const useTodo = ()=>{
+    return useContext(TodoContext);
+}
+
+export  const TodoContextProvider = TodoContext.Provider;
+
