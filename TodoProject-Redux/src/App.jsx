9aed6751a9e85c1b8a5a0924d0aef5ee7c58fd5f
Pravalika <pravalikareddymaccha@gmail.com>
2025-06-03 23:50:15 +0530

    TodoProject-Redux

---
+++ TodoProject-Redux/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AddToDo from './Components/AddTodo.jsx'
+import Todo from './Components/Todo.jsx'
+function App() {
+  
+  return (
+    <>
+     <AddToDo/>
+     <Todo/>
+    </>
+  )
+}
+
+export default App
